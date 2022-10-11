6339c9b3b61246440c335e0bd64ef90b0ca6c20b
jxav22 <94942712+ggeretsae@users.noreply.github.com>
2022-10-11 15:22:24 +1300

    feat: words are loaded from the csv file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,16 @@
+import csv
+from pathlib import Path
+
 # Load words from the csv file
+file_location = '../resources/category_difficulty.csv'
+path = Path(__file__).parent / file_location
+
+words = []
+
+with path.open() as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        words.append(row[0])
 
 # Scrape definitions
 
